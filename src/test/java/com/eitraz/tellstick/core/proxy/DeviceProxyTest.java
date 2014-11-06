10458c7e837fa49a4daa34992d07d8b74fcce2b4
Eitraz <bitbucket@eitraz.com>
2014-11-06 21:06:40 +0100

    Improved proxy test

--- src/test/java/com/eitraz/tellstick/core/proxy/DeviceProxyTest.java
+++ src/test/java/com/eitraz/tellstick/core/proxy/DeviceProxyTest.java
@@ -8,6 +8,7 @@ import java.util.concurrent.Callable;
 
 import static com.jayway.awaitility.Awaitility.await;
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertEquals;
 
 public class DeviceProxyTest {
 
@@ -15,16 +16,24 @@ public class DeviceProxyTest {
     public void testProxy() throws Exception {
         final TestDevice device = new TestDevice(1, "Test");
 
+        // Setup
+        DeviceProxy.getInstance().setCallDelay(500);
         DeviceProxy.getInstance().setTries(3);
 
+        // Run
         DeviceProxy.proxy(device, OnOffDevice.class).on();
 
+        // Wait for all tries
         await().until(new Callable<Integer>() {
             @Override
             public Integer call() throws Exception {
                 return device.getCounter();
             }
         }, equalTo(3));
+
+        // Make sure there is no other tries
+        Thread.sleep(750);
+        assertEquals(3, device.getCounter());
     }
 
 }
