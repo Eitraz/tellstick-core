9ecff80b13f880fb11d9d41f289bc38df3e66c51
Eitraz <bitbucket@eitraz.com>
2014-11-12 21:52:24 +0100

    DeviceHandlerTest added

---
+++ src/test/java/com/eitraz/tellstick/core/device/DeviceHandlerTest.java
@@ -0,0 +1,51 @@
+package com.eitraz.tellstick.core.device;
+
+import com.eitraz.tellstick.core.Tellstick;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class DeviceHandlerTest {
+    private Tellstick tellstick = new Tellstick();
+
+    @Before
+    public void before() {
+        tellstick.start();
+    }
+
+    @After
+    public void after() {
+        tellstick.stop();
+    }
+
+    @Test
+    public void testCreateDevice() throws Exception {
+        Device device = null;
+        try {
+            int numDevices = tellstick.getDeviceHandler().getDevices().size();
+
+            Map<String, String> parameters = new HashMap<>();
+            parameters.put("house", "A");
+            parameters.put("unit", "2");
+            device = tellstick.getDeviceHandler().createDevice("Test Device", "codeswitch", "arctech", parameters);
+
+            System.out.println("New device: " + device);
+
+            assertEquals(numDevices + 1, tellstick.getDeviceHandler().getDevices().size());
+
+            assertTrue(tellstick.getDeviceHandler().removeDevice(device));
+            assertEquals(numDevices, tellstick.getDeviceHandler().getDevices().size());
+            device = null;
+        } finally {
+            if (device != null) {
+                tellstick.getDeviceHandler().removeDevice(device);
+            }
+        }
+    }
+}
